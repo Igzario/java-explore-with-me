4111dac709b335b77d0b3c12850446ed0dc59d61
Igzario <88246005+Igzario@users.noreply.github.com>
2023-09-25 00:09:04 +0300

    diplom - part2 - 1st commit -fixGit

--- stats-server/src/main/java/ru/practicum/statistics/repository/StatisticRepository.java
+++ stats-server/src/main/java/ru/practicum/statistics/repository/StatisticRepository.java
@@ -2,7 +2,6 @@ package ru.practicum.statistics.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import ru.practicum.statistics.dto.HitDto;
 import ru.practicum.statistics.model.Hit;
 
@@ -11,17 +10,17 @@ import java.util.List;
 
 public interface StatisticRepository extends JpaRepository<Hit, Long> {
 
-    @Query(value="SELECT MAX(e.app) AS app, e.uri AS uri, COUNT(e.uri) AS hits " +
+    @Query(value = "SELECT MAX(e.app) AS app, e.uri AS uri, COUNT(e.uri) AS hits " +
             "FROM hits AS e " +
             "WHERE e.timestamp between ?1 AND ?2 " +
             "GROUP BY e.uri", nativeQuery = true)
     List<HitDto> countTotalperUri(LocalDateTime start, LocalDateTime end);
 
-    @Query(value="SELECT MAX(e.app) AS app, e.uri AS uri, COUNT(e.uri) AS hits " +
+    @Query(value = "SELECT MAX(e.app) AS app, e.uri AS uri, COUNT(e.uri) AS hits " +
             "FROM (SELECT DISTINCT ip AS i, * " +
             "FROM hits) AS e " +
             "WHERE e.timestamp between ?1 AND ?2 " +
             "GROUP BY e.uri", nativeQuery = true)
-    List<HitDto>countTotalperUriUniqueIp(LocalDateTime start, LocalDateTime end);
+    List<HitDto> countTotalperUriUniqueIp(LocalDateTime start, LocalDateTime end);
 
 }
